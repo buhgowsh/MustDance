7f390ae540bc1075924f93ed2d144e3f0499d1c0
Josh Sawyer <jawshs08@gmail.com>
2025-10-25 00:16:41 -0400

    separate backend and frontend sections

---
+++ backend/agent.py
@@ -0,0 +1,7 @@
+# we want an agent to pull up a video of the dance desired
+
+# we also want an agent to analyze the comparison in ddr accuracy style
+    # providing feedback on improvement if needed
+
+# curious about speech recognition to allow for searching for dances
+    # stretch goal: search for dances and music to dance to separately
